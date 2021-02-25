0535a79222e67ab43aa7763cbf896e8773efb4ea
StevenGrobe <steven.grobe@le-campus-numerique.fr>
2021-02-25 16:17:32 +0100

    exo 3 WIP

--- exo-3/script.js
+++ exo-3/script.js
@@ -5,3 +5,8 @@ console.log('----------');
 // Faites toutes vos manipulations de DOM ici
 
 // Et pas dans votre fichier HTML ! 
+
+$(document).ready(function () {
+    $('h1').text("Steven")
+    $('#tagline').text("J'apprends JS")
+})
