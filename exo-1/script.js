b1d99a850fcec493ab745f9eb9f441f57a3307d0
StevenGrobe <steven.grobe@le-campus-numerique.fr>
2021-02-25 16:16:31 +0100

    exo-1 done

--- exo-1/script.js
+++ exo-1/script.js
@@ -2,3 +2,19 @@ console.log('----------');
 console.log('Exercice 1');
 console.log('----------');
 
+
+// JQuery est une bibliotheque de JS, utilise des fonctions qui rends le code moins long et plus lisible (dom, requete ajax)
+// le jquery peut etre fait en javascript de base mais ca requiere plus de ligne
+
+$(document).ready(function () {
+    console.log("Je peux maintenant écrire du code jQuery");
+});
+
+//Expliquer (avec un commentaire) les lignes de code ci-dessus.
+// Quand ma page html est chargé, affiche "Je peux maintenant écrire du code jQuery" dans la console
+
+//En quoi cette ligne est indispensable ?
+// Car on vérifie que la page est bien chargée
+
+//Quel symbole permet de repérer dans un programme Javascript que du jQuery est utilisé ? (attention, ce n'est pas toujours le cas...)
+// Le symbole $
