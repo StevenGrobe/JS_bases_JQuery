b2d24c90de75308cf20c0918f3d0870ac8f786c3
StevenGrobe <grobesteven@gmail.com>
2021-02-26 11:50:49 +0100

    exo-1 RFV

--- exo-1/script.js
+++ exo-1/script.js
@@ -3,7 +3,10 @@ console.log('Exercice 1');
 console.log('----------');
 
 
-// JQuery est une bibliotheque de JS, utilise des fonctions qui rends le code moins long et plus lisible (dom, requete ajax)
+// JQuery est une bibliotheque de JS, utilise des fonctions qui rends le code moins long et plus lisible.
+// jQuery a été créée pour simplifier la manipulation du DOM HTML, la gestion d'événements, la création d'animations CSS et l'utilisation d'AJAX.
+// un code JavaScript respectant les normes imposées par jQuery sera compatible sur tous les navigateurs web
+
 // le jquery peut etre fait en javascript de base mais ca requiere plus de ligne
 
 $(document).ready(function () {
