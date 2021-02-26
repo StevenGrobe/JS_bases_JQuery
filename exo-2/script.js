319afea4e3602d899dec45501ea1da5ced5cbec9
StevenGrobe <grobesteven@gmail.com>
2021-02-26 11:51:09 +0100

    exo-2 RFV

--- exo-2/script.js
+++ exo-2/script.js
@@ -3,14 +3,14 @@ console.log('Exercice 2');
 console.log('----------');
 
 $(document).ready(function () {
-    $('#title') // Sélectionner le titre du blog : "John's Blog".
-    $('.sidenav *') // Sélectionner tous les éléments du sidemenu de gauche.
-    $('footer') // Sélectionner le footer.
-    $('.sidenav > .input-group') // Sélectionner l'élément qui permet de faire la barre de recherche.
-    $('.glyphicon') // Sélectionner tous les glyphicons de la page.
-    $('.label-success') // Sélectionner tous les labels verts.
+    $('#title'); // Sélectionner le titre du blog : "John's Blog".
+    $('.sidenav *'); // Sélectionner tous les éléments du sidemenu de gauche.
+    $('footer'); // Sélectionner le footer.
+    $('.sidenav > .input-group'); // Sélectionner l'élément qui permet de faire la barre de recherche.
+    $('.glyphicon'); // Sélectionner tous les glyphicons de la page.
+    $('.label-success'); // Sélectionner tous les labels verts.
     $('p.intro') // Sélectionner tous les paragraphes qui ont comme classe 'intro'.
-    $('img, .glyphicon-time') // Sélectionner toutes les images avec toutes les icones dont la classe est 'glyphicon-time'
-    $('.sidenav > :first-child') //  Sélectionner le premier élément du sidemenu.
-    $('p:even') // Bonus : sélectionner un paragraphe sur deux.
+    $('img, .glyphicon-time'); // Sélectionner toutes les images avec toutes les icones dont la classe est 'glyphicon-time'
+    $('.sidenav > :first-child'); //  Sélectionner le premier élément du sidemenu.
+    $('p:even'); // Bonus : sélectionner un paragraphe sur deux.
 })
