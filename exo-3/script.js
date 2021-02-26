0d6e109a7a91f3c5a66055ad8b9ba6902003f6b3
StevenGrobe <grobesteven@gmail.com>
2021-02-26 11:51:21 +0100

    exo-3 bonus à faire

--- exo-3/script.js
+++ exo-3/script.js
@@ -7,6 +7,22 @@ console.log('----------');
 // Et pas dans votre fichier HTML ! 
 
 $(document).ready(function () {
-    $('h1').text("Steven")
+    $('h1').text('Steven')
     $('#tagline').text("J'apprends JS")
+    $('#myNavbar ul:first').remove();
+    $('.glyphicon').removeClass('glyphicon glyphicon-log-in').addClass('bi bi-arrow-bar-left');
+    $('footer p').html('<b>Copyright 2021</b>');
+    $('div h3').text('Mes projets')
+    $('.container-fluid .col-sm-3 p').text('Mon projet');
+    $('img').attr('src', 'https://source.unsplash.com/random/225x125');
+    $('#work1').before(`
+    <div id='work1' class='row'>
+        <div class='col-sm-3'>
+            <p>Mon dernier projet</p>
+            <img id="last-project" src='https://source.unsplash.com/random/500x250'>
+        </div>
+    </div>
+    `)
+    $('footer').addClass('intro');
+
 })
