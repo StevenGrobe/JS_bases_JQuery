358c029dbefab8f5271d3ffe38143580729c2c5d
StevenGrobe <grobesteven@gmail.com>
2021-03-02 14:37:31 +0100

    exo-7 PFV

--- exo-7/script.js
+++ exo-7/script.js
@@ -2,29 +2,24 @@ console.log("exercice 7");
 
 function getData() {
   return data; // data is defined in DATA.js file
-} 
-
-function addLineToTable(data) { //
-  let tr = $("<tr/>");
-  let td = $("<td/>");
-
-  td.text(data.email);
-  tr.append(td);
-  td = $("<td/>");
-  
-  td.text(data.name.last);
-  tr.append(td);
-  td = $("<td/>");
-  
-  td.text(data.name.first);
-  tr.append(td);
-  td = $("<td/>");
-  
-  td.text(data.phone);
-  tr.append(td);
-  $("tbody").append(tr);
-
 }
-getData().forEach(data => {
-  addLineToTable(data);
+
+function addLineToTable (newUser) { //Ajoute une nouvelle ligne dans la balise tbody
+  $('tbody').append(`
+    <tr>
+      <td>${newUser.email}</td>
+      <td>${newUser.name.last}</td>
+      <td>${newUser.name.first}</td>
+      <td>${newUser.phone}</td>
+    </tr>
+  `);
+} // Append() permet d'insérer du contenu (texte, html)
+
+// Boucle forEach qui va appeler addLineToTable pour afficher les données de data
+$(document).ready(function () {
+  getData().forEach(newUser => { 
+    addLineToTable(newUser); 
+  });
 });
+
+console.log(data);
