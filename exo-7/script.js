90f1775800bb448c3115fc8c6cb87e26f33d2aa6
StevenGrobe <grobesteven@gmail.com>
2021-03-02 14:00:57 +0100

    exo-7 wip

--- exo-7/script.js
+++ exo-7/script.js
@@ -2,4 +2,29 @@ console.log("exercice 7");
 
 function getData() {
   return data; // data is defined in DATA.js file
+} 
+
+function addLineToTable(data) { //
+  let tr = $("<tr/>");
+  let td = $("<td/>");
+
+  td.text(data.email);
+  tr.append(td);
+  td = $("<td/>");
+  
+  td.text(data.name.last);
+  tr.append(td);
+  td = $("<td/>");
+  
+  td.text(data.name.first);
+  tr.append(td);
+  td = $("<td/>");
+  
+  td.text(data.phone);
+  tr.append(td);
+  $("tbody").append(tr);
+
 }
+getData().forEach(data => {
+  addLineToTable(data);
+});
