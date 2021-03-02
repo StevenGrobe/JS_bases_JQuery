3174500173b61d948bf66d615fafd8722eda8311
StevenGrobe <grobesteven@gmail.com>
2021-03-02 09:16:01 +0100

    exo-6 RFV

--- exo-6/script.js
+++ exo-6/script.js
@@ -9,24 +9,24 @@ function isEmail(email) {
 $("#subBtn").click(function () {
     // Verification Email
     if (!$('#mail').val()) { // On vérifie la valeur de l'id mail
-        $("#mailAlert").fadeIn();
+        $("#mailAlert").fadeIn(); // Montre l'alerte
         $("#mailAlert").text("email obligatoire"); // Affiche alerte en ajoutant du texte à id mailAlert
     } else if (!isEmail($('#mail').val())) { // Vérification regex
         $("#mailAlert").fadeIn();
         $("#mailAlert").text("Veuillez entrer un email valide!");
     } else {
-        $("#mailAlert").fadeOut();
+        $("#mailAlert").fadeOut(); // Cache l'alerte
     }
 
     // Verification Mot de passe 
     if (!$("#mdp").val()) { // On vérifie la valeur de l'id mdp
-        $("#mdpAlert").fadeIn();
+        $("#mdpAlert").fadeIn(); // Montre l'alerte
         $("#mdpAlert").text("Mot de passe obligatoire");
     } else if ($("#mdp").val().length < 6) { // Vérifie la taille du mdp < à 6
         $("#mdpAlert").fadeIn();
         $("#mdpAlert").text("Veuillez entrer un mot de passe de + 6 caractères");
     } else {
-        $("#mdpAlert").fadeOut();
+        $("#mdpAlert").fadeOut(); // Cache l'alerte
     }
 
     // Verification des champs
